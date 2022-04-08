48fa041c86f69bd6fa41d485899f25c64cc15b86
ArmidaGL <armida.gl@sjuanrio.tecnm.mx>
2022-04-08 11:20:52 -0500

    Update Ejemplo_ESP32Cam_Operadores.ino
    Ya complilado y probado de ambas formas, funciona correctamente

--- Ejemplo_ESP32Cam_Operadores/Ejemplo_ESP32Cam_Operadores.ino
+++ Ejemplo_ESP32Cam_Operadores/Ejemplo_ESP32Cam_Operadores.ino
@@ -7,23 +7,34 @@
  */
 
 // Bibliotecas
+//# include <Serial.h>
 
 // Constantes
 
 // Variables
+int dato=0;
 
 // Definición de objetos
 
 // Condiciones iniciales - Se ejecuta sólo una vez al energizar
 void setup() {// Inicio de void setup ()
   // Aquí va tu código
-
+  Serial.begin(115200); //Inicialización del monitor serial para el ESP32Cam
+  Serial.println("Conexión iniciada");// mensaje de prueba
+  delay(1000);//Espera
 }// Fin de void setup
 
 // Cuerpo del programa - Se ejecuta constamente
-void loop() {// Inicio de void loop
+void loop() {// Inicio de void loop                                                   
   // put your main code here, to run repeatedly:
-
+  dato++;
+  //if (dato > 8) {
+  //  dato = 0;
+  //}
+  dato%=8;
+  Serial.print("Este es el Dato: ");
+  Serial.println(dato);
+  delay(1000);//Espera
 }// Fin de void loop
 
 // Funcioes del usuario

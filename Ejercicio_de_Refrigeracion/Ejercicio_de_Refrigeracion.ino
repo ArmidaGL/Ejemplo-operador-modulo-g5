0e56b514c97806da79940b41fb3274330a67c841
ArmidaGL <armida.gl@sjuanrio.tecnm.mx>
2022-05-11 00:03:49 -0500

    Update WiFi y Broker
    Actualización conectado a WiFi, conectado a Broker público y publicando la temperatura

--- Ejercicio_de_Refrigeracion/Ejercicio_de_Refrigeracion.ino
+++ Ejercicio_de_Refrigeracion/Ejercicio_de_Refrigeracion.ino
@@ -33,17 +33,27 @@
 
 // Bibliotecas
 #include <WiFi.h>  // Biblioteca para el control de WiFi
+#include <PubSubClient.h> //Biblioteca para conexion MQTT
 #include "DHT.h" // Incluir la biblioteca de DHT11
 
 //Datos de WiFi
 const char* ssid = "CASTILLO";  // Aquí debes poner el nombre de tu red
 const char* password = "YolandaC23";  // Aquí debes poner la contraseña de tu red
 
+//Datos del broker MQTT
+//Puse un Broker IP público; para actualizar con: nslookup broker.hivemq.com en terminal ubuntu
+const char* mqtt_server = "35.157.221.58"; 
+const char* topicTemp= "CodigoIoT/IndustriaAlimentaria/Temperatura";
+IPAddress server(35,157,221,58);
+
+
+
 // Constantes
 // Constantes para manejar el DHT11
 #define DHTPIN 12       // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT11   // DHT 11
-#define TemperaturaAlta 29 // Límite de temperatura y que no ocupe memoria
+#define TemperaturaAlta 40 // Límite de temperatura y que no ocupe memoria
+
 
 const int BOTON1 = 13; // Manual
 const int BOTON2 = 15; // Alta Demanda
@@ -51,19 +61,19 @@ const int BOTON3 = 14; // Sobrecarga de Funcionamiento
 
 const int LEDM = 2; // Refrigeración MANUAL
 const int LEDA = 4; // Refreigeración Automática
-//const float TemperaturaAlta = 28;
 
-// Variables
-int dato1;
-int dato2;
-int dato3;
-float t;
-double TiempoNoBloqueante;
-double CambiaTiempo=0;
 
 // Definición de objetos
 WiFiClient espClient; // Este objeto maneja los datos de conexion WiFi
+PubSubClient client(espClient); // Este objeto maneja los datos de conexion al broker
 DHT dht(DHTPIN, DHTTYPE);   // Objeto para manejar el DHT11
+
+
+// Variables
+int dato1, dato2, dato3;
+float t, tant=0,timeNow,timeLast=0;
+int wait = 5000; //Tiempo de espera para madar mensajes por MQTT
+char dataString[8]; // Define una arreglo de caracteres para enviarlos por MQTT, especifica la longitud del mensaje en 8 caracteres
 
 // Condiciones iniciales - Se ejecuta sólo una vez al energizar
 
@@ -83,7 +93,7 @@ void setup() {// Inicio de void setup ()
     delay (500);
   }
 
- // Cuando se haya logrado la conexión, el programa avanzará, por lo tanto, puede informarse lo siguiente
+ // Cuando se haya logrado la conexión, se informa lo siguiente
   Serial.println();
   Serial.println("WiFi conectado");
   Serial.print("Direccion IP: ");
@@ -101,36 +111,61 @@ void setup() {// Inicio de void setup ()
   dato2=0;
   dato3=0;
 
+  // Conexión con el broker MQTT
+  client.setServer(server, 1883); // Conectarse a la IP del broker en el puerto indicado
+  client.setCallback(callback); // Activar función de CallBack, permite recibir mensajes MQTT y ejecutar funciones a partir de ellos
+  delay(1500);  // Esta espera es preventiva, espera a la conexión para no perder información
+  
+  timeLast=millis(); //Para iniciar el control del tiempo
+
 }// Fin de void setup
 
 // Cuerpo del programa - Se ejecuta constamente
 void loop() {// Inicio de void loop
-  // put your main code here, to run repeatedly:
+
+   // put your main code here, to run repeatedly:
   while (WiFi.status() == WL_CONNECTED) { 
+   //Verificar siempre que haya conexión al broker
+  
+  if (!client.connected()) {
+    Serial.println("No esta conectado al broker");
+    reconnect();  // En caso de que no haya conexión, ejecutar la función de reconexión, definida despues del void setup ()
+  }// fin del if (!client.connected())
+  client.loop(); // Esta función es muy importante, ejecuta de manera no bloqueante las funciones necesarias para la comunicación con el broker
+    
   leeSensor();
   leeBotones();
   activaRefrigeraciones();
   }
   } // Fin de void loop
 
-// Funcioes del usuario
+
+
+// Funciones del usuario
+
  void leeSensor ()
  {
     // Read temperature as Celsius (the default)
     
-    TiempoNoBloqueante= millis();
-    if (TiempoNoBloqueante > CambiaTiempo)
-    {
+    timeNow= millis();
+    if (timeNow - timeLast > wait) {
+      timeLast = timeNow; // Actualización de seguimiento de tiempo
       t = dht.readTemperature();
+      dtostrf(t, 1, 2, dataString);
+      Serial.print(F("La temperatura publicada: "));
+      Serial.print(dataString);
+      Serial.println(F(" °C "));
+      Serial.print(F("En el tópico: "));
+      Serial.println(topicTemp);
+      client.publish(topicTemp, dataString); // Esta es la función que envía los datos por MQTT
       if (isnan(t)) { // Check if any reads failed and exit early (to try again).
        Serial.println(F("Failed to read from DHT sensor!"));
        return;
      }
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.println(F("°C "));
-  CambiaTiempo=TiempoNoBloqueante+2000;
- }
+    }
+  //Serial.print(F("%  Temperature: "));
+  //Serial.print(t);
+  //Serial.println(F("°C "));
  }
 
  void leeBotones ()
@@ -144,23 +179,72 @@ void loop() {// Inicio de void loop
  {
    //Para activar la refrigeración Automática
   if (t>=TemperaturaAlta) {
-  digitalWrite (LEDA, HIGH);}
+  digitalWrite (LEDA, HIGH);
+  //Publicar alerta de temperatura Alta
+  //dataString = 'TmAlert';
+  //client.publish(topicTemp, dataString); // Esta es la función que envía los datos por MQTT
+  
+  }
   if (t<TemperaturaAlta) {
   digitalWrite (LEDA, LOW);}
   
   //Para activar la refrigeración Manual
   digitalWrite (LEDM, !dato1); //Prender el led Refrigeración automática
-  Serial.print(F("Dato 1: "));
-  Serial.println (dato1);
   delay (200);
    
   // Alta demanda o sobre carga, se activa la refrigeración automática
   if (!dato2 || !dato3) {
       digitalWrite (LEDA, HIGH); //Prender el led refrigeración automática
-      Serial.print(F("Dato 2: "));
-      Serial.println (dato2);
-      Serial.print(F("Dato 3: "));
-      Serial.println (dato3);
       delay (200);
       }
   }
+
+// Esta función permite tomar acciones en caso de que se reciba un mensaje correspondiente a un tema al cual se hará una suscripción
+void callback(char* topic, byte* message, unsigned int length) {
+
+  // Concatenar los mensajes recibidos para conformarlos como una varialbe String
+  String messageTemp; // Se declara la variable en la cual se generará el mensaje completo  
+  for (int i = 0; i < length; i++) {  // Se imprime y concatena el mensaje
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+
+  // Se comprueba que el mensaje se haya concatenado correctamente
+  Serial.println();
+  Serial.print ("Mensaje concatenado en una sola variable: ");
+  Serial.println (messageTemp);
+
+  // En esta parte puedes agregar las funciones que requieras para actuar segun lo necesites al recibir un mensaje MQTT
+
+  if (String(topic) == topicTemp) {  // En caso de recibirse mensaje en el tema esp32/output
+    if(messageTemp == "true"){
+      Serial.println("Mandó mensaje");
+    }// fin del if topic 
+    else if(messageTemp == "false"){
+      Serial.println("Sin mensaje");
+    }// fin del else if(messageTemp == "false")
+  }// fin del if topic
+}// fin del void callback
+
+// Función para reconectarse
+void reconnect() {
+  // Bucle hasta lograr conexión
+  Serial.println("Entre a la función reconect");
+  while (!client.connected()) { // Pregunta si hay conexión
+    Serial.print("Tratando de contectarse...");
+    // Intentar reconexión
+    if (client.connect("espClient")) { //Pregunta por el resultado del intento de conexión
+      Serial.println("Conectado");
+      client.subscribe(topicTemp); // Esta función realiza la suscripción al tema
+    }// fin del  if (client.connect("espClient"))
+    else {  //en caso de que la conexión no se logre
+      Serial.print("Conexion fallida, Error rc=");
+      Serial.print(client.state()); // Muestra el codigo de error
+      Serial.println(" Volviendo a intentar en 5 segundos");
+      // Espera de 5 segundos bloqueante
+      delay(5000);
+      Serial.println (client.connected ()); // Muestra estatus de conexión
+    }// fin del else
+  }// fin del bucle while (!client.connected())
+}// fin de void reconnect
+  
